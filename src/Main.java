8d06a105f177488f28ce7f628111cd3e9e884d51
Lubneuski <aliaksandr.lubneuski@gmail.com>
2022-11-10 00:05:07 +0300

    100%

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    static Scanner scr= new Scanner(System.in);
 
+    public static void main(String[] args)  {
         task1();
         task2();
-
+        task3();
+        scr.close();
     }
 
     private static void task1() {
@@ -25,6 +28,7 @@ public class Main {
 
     private static void task2() {
         System.out.println("Задание 2. Написать программу для вывода на экран дату следующего вторника.");
+
         int dd=1;       //день
         int mm=10;      //месяц от 0 до 11
         int yyyy=2022;  //год
@@ -40,4 +44,30 @@ public class Main {
         System.out.println();
     }
 
+    private static void task3() {
+        System.out.println("Задание 3. Создать обобщенный функциональный интерфейс. Написать класс с одним методом.\n" +
+                "В этом методе реализуйте логику: - если в консоль введена цифра 1, то: \n" +
+                "использования интерфейса со строковым типом и передать в метод интерфейса логику реверса строки(вывода строки в обратном порядке).");
+
+        System.out.println("Введите число 1 для реверса строки:");
+        String res ="Приветики, давай дружить!";
+        try {
+            if (scr.hasNextInt()){
+            int t = scr.nextInt();
+                if (t == 1) {
+                    Revers rev = new Revers() {
+                        @Override
+                        public String rere(String str) {
+                            return new StringBuilder(str).reverse().toString();
+                        }
+                    };
+                    System.out.println(rev.rere(res));
+                } else System.out.println("Введите число 1 в следующий раз.");
+            }else{
+                System.out.println("Введено не число");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
